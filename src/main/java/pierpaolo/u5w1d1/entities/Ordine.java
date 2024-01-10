64bda9ed1148ed58d80d8c540c5e6b13a982bf13
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2024-01-10 21:03:38 +0100

    add other test

--- src/main/java/pierpaolo/u5w1d1/entities/Ordine.java
+++ src/main/java/pierpaolo/u5w1d1/entities/Ordine.java
@@ -17,8 +17,8 @@ public class Ordine {
     private LocalTime ora;
     private List<Prodotto> prodottiOrdinati;
     private Tavolo tavolo;
-    @Value("${costo.coperto}")
-    private double costoCoperto;
+//    @Value("${costo.coperto}")
+//    private double costoCoperto;
 
     public Ordine(int numCoperti, List<Prodotto> prodottiOrdinati, Tavolo tavolo) {
         this.numCoperti = numCoperti;
@@ -32,7 +32,7 @@ public class Ordine {
     public double getTotale(){
         return this.prodottiOrdinati.stream().mapToDouble(Prodotto::getPrezzo).sum()+ this.numCoperti + this.tavolo.getCostoCoperto();
     }
-    public double getCostoCoperto(){
-        return this.numCoperti * this.costoCoperto;
-    }
+//    public double getCostoCoperto(){
+//        return this.numCoperti * this.costoCoperto;
+//    }
 }
--- src/test/java/pierpaolo/u5w1d1/classeTest.java
+++ src/test/java/pierpaolo/u5w1d1/classeTest.java
@@ -2,40 +2,65 @@ package pierpaolo.u5w1d1;
 
 import org.junit.jupiter.api.*;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import pierpaolo.u5w1d1.entities.Ordine;
-import pierpaolo.u5w1d1.entities.Pizza;
-import pierpaolo.u5w1d1.entities.Prodotto;
+import pierpaolo.u5w1d1.entities.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class classeTest {
-    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(U5w1d1Application.class);
+    private static AnnotationConfigApplicationContext ctx ;
     @BeforeAll
     public static void beforeAll() {
         System.out.println("BEFORE ALL");
+        ctx = new AnnotationConfigApplicationContext(U5w1d1Application.class);
     }
     @Test
     public void testSum() {
-        System.out.println("TEST 1");
+        System.out.println("TEST 1 sum");
         int result = 4;
         assertEquals(4, result);
     }
     @Test
     void margheritaTest(){
+        System.out.println("TEST 2 margherita");
         Pizza marg = ctx.getBean("margherita",Pizza.class);
         assertEquals(8.97, marg.getPrezzo());
     }
 
     @Test
     void margheritaTest2(){
+        System.out.println("TEST 3 margherita");
         Prodotto marg = (Prodotto) ctx.getBean("margherita");
         assertEquals(8.97, marg.getPrezzo());
     }
-
     @Test
-    void ordineTest(){
-        Ordine o = ctx.getBean(Ordine.class);
-        assertEquals(5, o.getTotale());
+    void pomodorotest(){
+        System.out.println("TEST 4 pomodoro");
+        Topping t = ctx.getBean("pomodoro", Topping.class);
+        assertEquals(0.99,t.getPrezzo());
     }
+    @Test
+    void margheritaIsNotXl(){
+        System.out.println("TEST 5 is not xl");
+        Pizza m = ctx.getBean("margherita", Pizza.class);
+        assertFalse(m.isXl());
+    }
+    @Test
+    void margheritaIsXl(){
+        System.out.println("TEST 6 is xl");
+        Pizza m = ctx.getBean("margheritaXl", Pizza.class);
+        assertTrue(m.isXl());
+    }
+    @Test
+    void costoCopertoAppProperties(){
+        System.out.println("TEST 7 application properties");
+        Tavolo t = ctx.getBean("tavolo_1", Tavolo.class);
+        assertEquals(2.00,t.getCostoCoperto());
+    }
+
+//    @Test
+//    void ordineTest(){
+//        Ordine o = ctx.getBean(Ordine.class);
+//        assertEquals(5, o.getTotale());
+//    }
 
 }
